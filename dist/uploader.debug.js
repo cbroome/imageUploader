517d4356c92a672c4a6cfc3c86957c3e7ad2f563
Chris Broome <chris.a.broome@gmail.com>
2015-08-03 12:31:03 -0400

    Boilerplate AMD-plugin header

---
+++ dist/uploader.debug.js
@@ -0,0 +1,15 @@
+(function (factory) {
+if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module depending on jQuery.
+    define(['jquery'], factory);
+} else {
+    // No AMD. Register plugin with global jQuery object.
+    factory(jQuery);
+}
+}(function ($) {
+
+    $.fn.yourjQueryPlugin = function () {
+        // Put your plugin code here
+    };  
+
+}));
